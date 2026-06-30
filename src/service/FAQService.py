5e5fe8bcafb7dad7010758c1e00dc0974d05d373
subinss838 <subinss838@gmail.com>
2026-06-30 19:39:00 +0900

    기능: [KAN-37] FAQ 서비스 조회 및 데이터 적재 함수 작성

---
+++ src/service/FAQService.py
@@ -0,0 +1,61 @@
+"""FAQ"""
+
+import csv
+
+from src.type.faq_item import FAQItem
+from src.type.manufacturer import Manufacturer
+from src.type.page import Page
+from src.repository.repository import find_faq, create_faq
+
+
+def get_faq(page: int, size: int, manufacturer: Manufacturer) -> Page:
+    """
+    Args:
+        page: 조회할 페이지 번호
+        size: 한 페이지에서 가져올 FAQ개수
+        manufacturer: 조회 대상 제조사
+        
+    Returns:
+        조회된 FAQ 목록과 페이지 정보를 담은 Page 객체
+    """
+
+    category = manufacturer.value
+    sub_category = page
+
+    faq_items: list[FAQItem] = find_faq(category=category, sub_category=sub_category)
+
+    total_count = len(faq_items)
+    total_page = (total_count + size -1) // size if size > 0 else 0
+
+    start = (page -1) * size
+    end = start + size
+    paged_items = faq_items[start:end]
+
+    return Page(
+        item = paged_items,
+        total_page = total_page,
+        current_page = page
+    )
+
+def set_faq(file_path: str) -> None:
+    """
+    Args:
+        file_path: 적재할 FAQ CSV 파일의 경로.
+
+    Returns:
+        없음.
+    """
+
+
+    with open(file_path, encoding="utf-8-sig") as csv_file:
+        reader = csv.DictReader(csv_file)
+
+        for row in reader:
+            faq_item = FAQItem(
+                manufacturer=Manufacturer(row["manufacturer"]),
+                category=row["category"],
+                question=row["question"],
+                answer=row["answer"],
+                source_url=row["source_url"],
+            )
+            create_faq(faqitem=faq_item)
