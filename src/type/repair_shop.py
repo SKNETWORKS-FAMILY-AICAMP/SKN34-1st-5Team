66deef4c5ed7225a5eef30dd66eb1ea6cf2796af
Amber8800 <silveresc88@gmail.com>
2026-07-01 20:27:12 +0900

    fix:[KAN-59] RepairShopType Enum클래스 요약정보 추가

--- src/type/repair_shop.py
+++ src/type/repair_shop.py
@@ -2,4 +2,8 @@ from dataclasses import dataclass
 
 @dataclass
 class RepairShop(Location):
+    """정비소의 타입을 구분하는 데이터 클래스
+    Attributes:
+      repair_shop_type: 정비소 타입을 구분하는 항목
+    """
     repair_shop_type : RepairShopType
