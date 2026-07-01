f8886d98b5e21b0a54e6c679775230c1ea2d3504
Amber8800 <silveresc88@gmail.com>
2026-07-01 20:26:18 +0900

    fix: [KAN-57] RepairShop 데이터 클래스 요약정보 추가

--- src/type/repair_shop_type.py
+++ src/type/repair_shop_type.py
@@ -1,6 +1,11 @@
 import enum
 
 class RepairShopType(enum):
+    """전기차 정비소 유형 정보가 담긴 ENUM 클래스
+    Attributes:
+     HYUNDAI_BLUEHANDS: 현대 블루핸즈 정비소를 의미
+     KIA_AUTO_Q: 기아 오토큐 정비소를 의미
+    """
 
     HYUNDAI_BLUEHANDS = "블루핸즈"
     KIA_AUOT_Q = "기아 오토큐"
