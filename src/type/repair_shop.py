ccf957f186e0a58bb4c356c20e3f8a60d8a73449
Amber8800 <silveresc88@gmail.com>
2026-07-02 00:48:19 +0900

    fix:[KAN-57] RepairShop 정비 범위 항목 추가

--- src/type/repair_shop.py
+++ src/type/repair_shop.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
+from src.type.location import Location
+from src.type.repair_shop_type import RepairShopType
 
 @dataclass
 class RepairShop(Location):
     """정비소의 타입을 구분하는 데이터 클래스
     Attributes:
       repair_shop_type: 정비소 타입을 구분하는 항목
+      repair_scope: 정비소 범위를 나타내는 항목
     """
     repair_shop_type : RepairShopType
+    repair_scope : str
