94e4b4f4ad0272c9ea6526cccc39951e38438e26
wind1484 <wind14849031@gmail.com>
2026-07-01 18:40:42 +0900

    fix: [KAN-49] ElectricVehicle 데이터 클래스 생성

--- src/type/electric_vehicle.py
+++ src/type/electric_vehicle.py
@@ -1,13 +1,24 @@
 from src.type.manufacturer import Manufacturer
+from src.type.charging_type import ChargingType
 
 class ElectricVehicle:
 
-    def __init__(self):
-        self.manufacturer: Manufacturer
-        self.model_name: str
-        self.trim_name: str
-        self.price: int
-        self.driving_range: int
-        self.efficiency: float
-        self.slow_charging_type: ChargingType
-        self.fast_charging_type: ChargingType
+    def __init__(
+            self,
+            manufacturer: Manufacturer,
+            model_name: str,
+            trim_name: str,
+            price: int,
+            driving_range: int,
+            efficiency: float,
+            slow_charging_type: ChargingType,
+            fast_charging_type: ChargingType
+            ):
+        self.manufacturer: Manufacturer = manufacturer
+        self.model_name: str = model_name
+        self.trim_name: str = trim_name
+        self.price: int = price
+        self.driving_range: int = driving_range
+        self.efficiency: float = efficiency
+        self.slow_charging_type: ChargingType = slow_charging_type
+        self.fast_charging_type: ChargingType = fast_charging_type
