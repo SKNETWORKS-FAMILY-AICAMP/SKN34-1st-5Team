5867a08bc0820609d542b69a088b23cd74c04329
wind1484 <wind14849031@gmail.com>
2026-07-01 19:35:46 +0900

    [KAN-48] Subsidy 서비스 클래스 생성

--- src/service/subsidy_service.py
+++ src/service/subsidy_service.py
@@ -1,5 +1,6 @@
 """Subsidy 서비스 클래스 생성"""
 
+import csv
 
 from src.type.region import Region
 from src.type.electric_vehicle import ElectricVehicle
@@ -7,34 +8,23 @@ from src.type.subsidy import Subsidy
 from src.repository.repository import Repository
 
 
-class SubsidyService:
-    _instance = None
+def get_subsidy(
+    
+    region: Region,
+    electric_vehicle: ElectricVehicle
+    ) -> Subsidy:
+    """
+    지역과 전기차 정보를 기준으로 보조금 정보를 조회한다.
+    """
+    subsidy = Repository.find_subsidy_by_region_and_vehicle(
+        region,
+        electric_vehicle
+    )
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(SubsidyService, cls).__new__(cls)
-        return cls._instance
+    return subsidy
 
-    def __init__(self):
-        self.repository = Repository()
-
-    def get_subsidy(
-        self,
-        region: Region,
-        electric_vehicle: ElectricVehicle
-    ) -> Subsidy | None:
-        """
-        지역과 전기차 정보를 기준으로 보조금 정보를 조회한다.
-        """
-        subsidy = self.repository.find_subsidy_by_region_and_vehicle(
-            region,
-            electric_vehicle
-        )
-
-        return subsidy
-
-    def set_subsidy(self, file_path: str) -> None:
-        """
-        파일 경로를 받아 보조금 데이터를 등록한다.
-        """
-        self.repository.save_subsidy_from_file(file_path)
+def set_subsidy(file_path: str) -> None:
+    """
+    파일 경로를 받아 보조금 데이터를 등록한다.
+    """
+    Repository.save_subsidy_from_file(file_path)
