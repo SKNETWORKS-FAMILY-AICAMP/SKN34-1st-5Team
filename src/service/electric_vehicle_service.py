0de65529ed7ad14d58d56edee439fd19f3a6c9c3
wind1484 <wind14849031@gmail.com>
2026-07-02 09:02:09 +0900

    fix : [KAN-51] ElectricVehicleService 서비스 클래스 생성

--- src/service/electric_vehicle_service.py
+++ src/service/electric_vehicle_service.py
@@ -1,37 +1,45 @@
+
 """전기차 차량 서비스"""
+
 
 from src.repository.repository import Repository
 from src.type.electric_vehicle import ElectricVehicle
 from src.type.manufacturer import Manufacturer
 
 
-
 def get_all() -> list[ElectricVehicle]:
     """
+    DB에 저장된 전체 전기차 차량 목록을 조회한다.
+
     Returns:
-        전체 전기차 차량 목록
+        list[ElectricVehicle]: 전체 전기차 차량 목록
     """
 
     db = Repository()
 
-    vehicles: list[ElectricVehicle] = db.find_all_electric_vehicles()
+    vehicles: list[ElectricVehicle] = db.find_all_vehicle()
 
     return vehicles
 
 
-def get_by_vehicle(model_name: str, trim_name: str) -> ElectricVehicle | None:
+def get_by_vehicle(
+    model_name: str,
+    trim_name: str
+) -> ElectricVehicle | None:
     """
+    모델명과 트림명을 기준으로 특정 전기차 차량 정보를 조회한다.
+
     Args:
         model_name: 차량 모델명
         trim_name: 차량 트림명
 
     Returns:
-        모델명과 트림명에 해당하는 ElectricVehicle 객체
+        ElectricVehicle | None: 조회된 전기차 차량 정보
     """
 
     db = Repository()
 
-    vehicle: ElectricVehicle | None = db.find_electric_vehicle(
+    vehicle: ElectricVehicle | None = db.find_all_vehicle(
         model_name=model_name,
         trim_name=trim_name
     )
@@ -41,6 +49,8 @@ def get_by_vehicle(model_name: str, trim_name: str) -> ElectricVehicle | None:
 
 def set_vehicle(vehicle: ElectricVehicle) -> None:
     """
+    전기차 차량 정보를 DB에 등록한다.
+
     Args:
         vehicle: 등록할 ElectricVehicle 객체
 
@@ -50,17 +60,19 @@ def set_vehicle(vehicle: ElectricVehicle) -> None:
 
     db = Repository()
 
-    db.create_electric_vehicle(vehicle=vehicle)
+    db.create_vehicle(vehicle=vehicle)
 
 
 def get_manufacturer() -> list[Manufacturer]:
     """
+    등록된 제조사 목록을 조회한다.
+
     Returns:
-        제조사 Enum 목록
+        list[Manufacturer]: 제조사 목록
     """
 
     db = Repository()
 
-    manufacturers: list[Manufacturer] = db.find_manufacturer()
+    manufacturers: list[Manufacturer] = db.find_all_manufacturers()
 
     return manufacturers
